a553cf1ec8bf40b87b9e5bf4c3c8b22d34f64879
mikhail.eshchenko <mikhail.eshchenko@gmail.com>
2019-05-16 12:10:48 +0300

    add randomize and playAgain function

--- app.js
+++ app.js
@@ -10,7 +10,7 @@
 //Game values
 let min = 1,
     max = 10,
-    winningNum = 2,
+    winningNum = getRandomNum(min, max),
     guessesLeft = 3;
 
 // UI elements
@@ -24,6 +24,13 @@ const game = document.querySelector('#game'),
 //Assign UI min and max
 minNum.textContent = min;
 maxNum.textContent = max;
+
+//Play again event listener
+game.addEventListener('mousedown',function(e){
+  if(e.target.className === 'play-again'){
+    window.location.reload();
+  }
+})
 
 //Listen for guess
 guessBtn.addEventListener('click',function(e){
@@ -73,6 +80,10 @@ function gameOver(won, msg){
   guessInput.style.borderColor = color;
   //setMessage
   setMessage(msg, color);
+
+  //Play again?
+  guessBtn.value = 'Сыграть еще';
+  guessBtn.className += 'play-again';
 };
 
 //setMessage
@@ -80,3 +91,8 @@ function setMessage(msg, color){
   message.textContent = msg;
   message.style.color = color;
 };
+
+//getRandomNum
+function getRandomNum(min, max){
+  return Math.floor(Math.random()*(max - min + 1) + min);
+}
